951b98f7eb68c9dd5e20945b4d737706e3279d17
Flyli0 <kirillrozkov653@gmail.com>
2026-02-05 19:55:47 +0500

    Finished Key schedule for 128 196 bit done

--- AES/KeyExpansion.py
+++ AES/KeyExpansion.py
@@ -1,7 +1,7 @@
 from AES import secret
 from constants import Sbox
 from constants import Rcon
-#from AES import roundnum 11223344556677889900aabbccddeeff
+#11223344556677889900aabbccddeeff
 
 def rotWord(word):
     rot_word = word[1:] + word[:1]
@@ -14,35 +14,39 @@ def subWord(word):
 
 
 def rconXOR(word, roundnum):
-    word[0] = Rcon[roundnum][0]
-    word[1] = Rcon[roundnum][1]
-    word[2] = Rcon[roundnum][2]
-    word[3] = Rcon[roundnum][3]
+    return [b ^ r for b, r in zip(word, Rcon[roundnum])]
 
-def g(word,roundnum):
+
+def g(word, roundnum):
     rw = rotWord(word)
     sw = subWord(rw)
-    rconXOR(sw,roundnum)
+    sw = rconXOR(sw, roundnum)
     return bytes(sw)
+
 
 def xor_words(a, b):
     return bytes(x ^ y for x, y in zip(a, b))
+
 
 length = "128bit"
 Nb = 4  # const
 Nr = 10
 Nk = 4  # because 128 bit = 16 bytes = 4 words
 Nkeys = Nb * (Nr + 1)
-keys = []
+roundkeys = []
+key = []
+count = 0
 words = [secret[i:i + 4] for i in range(0, len(secret), 4)]
 for i in range(4, Nkeys):
     words.append(bytes(4))
-print(words)
 for i in range(4, Nkeys):
     if i % Nk != 0:
-        words[i] = xor_words(words[i-Nk],words[i-1])
-        print(0)
+        words[i] = xor_words(words[i - Nk], words[i - 1])
     else:
-        words[i] = xor_words(words[i-Nk],g(words[i-1],1))
+        words[i] = xor_words(words[i - Nk], g(words[i - 1], i//4))
 
-print(words)
+for i in range(0, len(words), 4):
+    roundkeys.append(b''.join(words[i:i+4]))
+
+
+print(roundkeys)
