9efb4c6dc0dd5c0ed6aaaf39d6c012203d861a59
Flyli0 <kirillrozkov653@gmail.com>
2026-02-05 22:19:13 +0500

    Subbytes added

--- AES/AES.py
+++ AES/AES.py
@@ -1,12 +1,16 @@
 from KeyExpansion import roundkeys
-
-#block_hex = input("Insert 16hex symbols \n")
-#block = bytes.fromhex(block_hex)
-#print(block)
-
+from KeyExpansion import Nr
+from SubBytes import subBytes
+block_hex = input("Insert 16 bytes \n")
+block = bytes.fromhex(block_hex)
+print(block)
 print('Success')
 
-#state = bytes(p^k for p,k in zip(block,secret)) #Тут создаю массив стейт который хорит сразу инишл ки и наш блок 128бит
+state = bytearray(p^k for p,k in zip(block,roundkeys[0])) #Тут создаю массив стейт который хорит сразу инишл ки и наш блок 128бит
+
+for i in range(Nr):
+    state = subBytes(state)
+
 
 
 
