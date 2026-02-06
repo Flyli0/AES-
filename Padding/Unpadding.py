8aa7ec3504833a9d9b5fdabf7011cdd7c5678a1a
Ibrayev Iskander <ibrayeviskander@MacBook-Pro-Ibrayev.local>
2026-02-06 14:09:06 +0500

    создал файл для анпаддинга, в нем создал функцию для анпаддинга

---
+++ Padding/Unpadding.py
@@ -0,0 +1,13 @@
+def unpadding_func(padded_plaintext, block_size):
+    if len(padded_plaintext) == 0 or len(padded_plaintext) % block_size != 0:
+        raise ValueError("Invalid padded plaintext length")
+
+    padding_len = padded_plaintext[-1]
+
+    if padding_len < 1 or padding_len > block_size:
+        raise ValueError("Invalid padding length")
+
+    if padded_plaintext[-padding_len:] != bytes([padding_len] * padding_len):
+        raise ValueError("Invalid padding")
+
+    return padded_plaintext[:-padding_len]
