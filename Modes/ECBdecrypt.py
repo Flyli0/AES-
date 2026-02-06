5aba1d932098b98b4993cb4ac8e49562895f77b2
Ibrayev Iskander <ibrayeviskander@MacBook-Pro-Ibrayev.local>
2026-02-06 18:38:33 +0500

    created files for ecb mode for encrypting and decrypting, created corresponding functions in them

---
+++ Modes/ECBdecrypt.py
@@ -0,0 +1,10 @@
+from AES.AESdecoding import decryption
+from Padding.Padding import unpadding_func
+
+def ecb_decrypt(ciphertext):
+    plaintext = b""
+    for i in range(0, len(ciphertext), block_size):
+        block = ciphertext[i:i+block_size]
+        plaintext += decryption(block)
+    plaintext = unpadding_func(plaintext, block_size)
+    return plaintext
---
+++ Modes/ECBencrypt.py
@@ -0,0 +1,10 @@
+from AES.AESencoding import cipher
+from Padding.Padding import padding_func
+
+def ecb_encrypt(plaintext):
+    plaintext = padding_func(plaintext, block_size)
+    ciphertext = b""
+    for i in range(0, len(plaintext), block_size):
+        block = plaintext[i:i+block_size]
+        ciphertext += cipher(block)
+    return ciphertext
